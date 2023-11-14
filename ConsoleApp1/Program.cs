7dd9ceb0c113098e19942343b6092d739e657d71
Dánjal Skaalum <danjal.skaalum@gmail.com>
2023-11-14 09:53:03 +0100

    Adding comments, fixing ConvertToCm error, altering according to teachers new additions.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,21 +8,39 @@ using System.Threading.Tasks;
 namespace ConsoleApp1;
 internal class Program
 {
+    // Constant - Value cannot be changed or modified. It is set in Compiling, not runtime.
     const double bananasize = 18;
+
+    // Static void Main is run at program startup. string[] args is used to
+    // intercept cli commands.
     static void Main(string[] args)
     {
+        // Small method that sets up the color and size. 
         SetupGui();
+
+        // Writes out the commandline interface (CLI) coammands.
+        // Foreach is one of the loops together with For, While and Do While.
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+
+        // Infinite loop (while(true)) There should ALWAYS be an exit
+        // strategy to exit a program.
         while(true)
         {
             StartHere();
             Console.WriteLine("Try again (Y/N)");
             if(Console.ReadKey().Key == ConsoleKey.N)
             {
+                //Environment.Exit(0);
                 break;
             }
             Console.Clear();
         }
-
+        /// <summary>
+        /// Our main method in the bananaconverter
+        /// </summary>
         static void StartHere()
         {
             Console.WriteLine("Convert Banana to cm.");
@@ -32,9 +50,17 @@ internal class Program
             ShowResult(amountbanana, result);
         }
 
+        /// <summary>
+        /// Gets User input andd converts to double
+        /// </summary>
+        /// <returns></returns>
         static double GetInput()
         {
             double amount;
+            // TryParse returns a bool. True if it can parse the string value,
+            // otherwise false. Since WHILE runs if true we false
+            // the method with logical NOT operator (!), os it becomes true
+            // if not possible to convert, thus continuing the loop.
             while (!double.TryParse(Console.ReadLine(), out amount))
             {
                 Console.WriteLine("Error. Try again.");
@@ -42,14 +68,27 @@ internal class Program
             return amount;
         }
 
+        /// <summary>
+        /// Simple calculation that multiplies amount of banana with standard banana size.
+        /// </summary>
+        /// <param name="banana"></param>
+        /// <returns>size in cm.</returns>
         static double ConvertToCm(double banana)
         {
             return bananasize * bananasize; 
         }
 
+        /// <summary>
+        /// Prints out the result in console.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="result"></param>
         static void ShowResult(double amountbanana, double result)
         {
-            Console.WriteLine(amountbanana + " banana equals " + result + " cm. with a standard size of banana equal " + bananasize);
+            //String interpolation examples
+            //Console.WriteLine("{0} banana equals {1} cm. with a standard size of banana equal {2}", amountbanana, result, bananasize);
+            string s2 = $"{amountbanana} banana equals {result} cm. with a standard size of banana equal {bananasize}";
+            Console.WriteLine(s2);
         }
     }
 
